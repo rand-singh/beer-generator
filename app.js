3315b07e9f2c82f9358c8f2a9b3e552a707192de
Randeep Singh <rand-singh@users.noreply.github.com>
2020-08-17 16:49:43 +0100

    Logic and styling
    Added logic to update data into the DOM, added beer icon, added styling

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-fetch("https://api.punkapi.com/v2/beers/random")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
+const startBtn = document.querySelector(".beer-button");
+const randomBeer = document.querySelector(".random-beer");
+const descriptionDisplay = document.querySelector(".description");
 
-    const name = data[0].name;
-    const description = data[0].description;
-    const { unit, value } = data[0].volume;
-  });
+const getData = (e) => {
+  e.preventDefault();
+  fetch("https://api.punkapi.com/v2/beers/random")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+
+      const name = data[0].name;
+      const description = data[0].description;
+      const { unit, value } = data[0].volume;
+
+      randomBeer.innerHTML = name + " " + value + " " + unit;
+      descriptionDisplay.innerHTML = description;
+    });
+};
+
+startBtn.addEventListener("click", getData);
