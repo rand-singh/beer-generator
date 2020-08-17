c082f565650afe3ff0a98ff041276c46126e67e0
Randeep Singh <rand-singh@users.noreply.github.com>
2020-08-17 16:07:23 +0100

    Fetch JSON, then console log the data

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('hello')
+fetch('https://api.punkapi.com/v2/beers')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        console.log(data);
+    })
