894a5570b912ed5e2d518a42160f9efdf3940fe2
Randeep Singh <rand-singh@users.noreply.github.com>
2020-08-17 16:12:50 +0100

    Extract required data

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-fetch('https://api.punkapi.com/v2/beers')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data);
-    })
+fetch("https://api.punkapi.com/v2/beers/random")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+
+    const name = data[0].name;
+    const description = data[0].description;
+    const { unit, value } = data[0].volume;
+  });
